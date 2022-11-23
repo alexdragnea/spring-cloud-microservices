0f6c21a8bbc16dfa4bebd9cff02e2d0882c7d8e5
Alexandru Dragnea <alexdg722@gmail.com>
2022-11-23 17:15:46 +0200

    fix checkstyle

--- raiting-service/src/test/java/net/dg/ratingservice/service/validation/RatingValidationServiceTest.java
+++ raiting-service/src/test/java/net/dg/ratingservice/service/validation/RatingValidationServiceTest.java
@@ -1,14 +1,13 @@
 package net.dg.ratingservice.service.validation;
 
 import net.dg.ratingservice.entity.Rating;
-import org.apache.commons.lang.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import javax.validation.ValidationException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @WebMvcTest(RatingValidationService.class)
 class RatingValidationServiceTest {
